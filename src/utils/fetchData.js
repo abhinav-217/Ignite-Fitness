05f48fb4312d339f4ea97bc8c7c4a79b0378d041
abhinav-saxena-03 <abhinav.saxena@ssipmt.com>
2023-07-28 22:30:55 +0530

    Done

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '4d350e58a6mshff82f15226b8dcap1586f7jsnadd764e9ac30',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
+
+export const youtubeOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4d350e58a6mshff82f15226b8dcap1586f7jsnadd764e9ac30',
+		'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+	}
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
